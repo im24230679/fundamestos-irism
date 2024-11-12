0e253e0f804534ba8f0cd7b5f2737af72fb0b1d4
im24230679 <L24230679@minatitlan.tecnm.mx>
2024-11-11 18:55:30 -0600

    Create Problema 2 .py

---
+++ Problema 2 .py
@@ -0,0 +1,9 @@
+# 2) Calcular el promedio de tres números e imprimir el resultado 
+
+cal1 = int(input("Ingresar Numero"))
+cal2 = int(input("Ingresar Numero"))
+cal3 = int(input("Ingresar Numero"))
+
+promedio = (cal1 + cal2 + cal3)/3
+
+print ("Resultado", promedio)
