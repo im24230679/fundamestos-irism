85f21a00426a512ddc9c6cab0ff317817b6d28b4
im24230679 <L24230679@minatitlan.tecnm.mx>
2024-11-11 18:56:03 -0600

    Update Problema 1 .py

--- Problema 1 .py
+++ Problema 1 .py
@@ -1,4 +1,4 @@
-# Hacer un programa que sume dos números y muestre el resultado 
+# 1) Hacer un programa que sume dos números y muestre el resultado 
 
 a = int(input("Ingresar Numero"))
 b = int(input("Ingresar Numero"))
