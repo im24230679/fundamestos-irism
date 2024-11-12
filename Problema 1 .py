655ec5edfa54cdd468f34dac8cf0c6b062462b82
im24230679 <L24230679@minatitlan.tecnm.mx>
2024-11-11 19:02:01 -0600

    Delete Problema 1 .py

--- Problema 1 .py
+++
@@ -1,8 +0,0 @@
-# 1) Hacer un programa que sume dos números y muestre el resultado 
-
-a = int(input("Ingresar Numero"))
-b = int(input("Ingresar Numero"))
-
-suma = a + b
-
-print("Resultado", suma)
